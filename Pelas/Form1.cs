4463d29aa6df441f174a7fb475d0d41d2c7fcfbb
iCPAC <cj.icpac@gmail.com>
2020-03-25 20:46:49 -0600

    Primeras Opciones

--- Pelas/Form1.cs
+++ Pelas/Form1.cs
@@ -16,5 +16,10 @@ namespace Pelas
         {
             InitializeComponent();
         }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
     }
 }
